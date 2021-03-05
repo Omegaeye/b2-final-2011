ff8d25279508b6446e8fb57bf40bd69a53f27288
Khoa Nguyen <khoang323@hotmail.com>
2021-03-05 09:36:31 -0700

    Complete flight index spec

--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe 'As visitor, when I visit flight index page' do
   before :each do
     @flight1 = Flight.create!(number: "1", date: "01/01/10", time: "10:00 AM", departure: "Denver", arrival: "Cancun")
     @flight2 = Flight.create!(number: "2", date: "01/01/10", time: "11:00 AM", departure: "Denver", arrival: "New York City")
-    @flight3 = Flight.create!(number: "3", date: "01/02/10", time: "9:00 AM", departure: "Manhattan", arrival: "San Franscisco")
-    @flight4 = Flight.create!(number: "4", date: "01/03/10", time: "1:00 PM", departure: "Phoenix", arrival: "Salt Lake City")
-    @flight5 = Flight.create!(number: "5", date: "01/03/10", time: "2:00 PM", departure: "Las Vegas", arrival: "Bahamas")
+    @flight3 = Flight.create!(number: "3", date: "01/01/10", time: "9:00 AM", departure: "Manhattan", arrival: "San Franscisco")
+    @flight4 = Flight.create!(number: "4", date: "01/01/10", time: "1:00 PM", departure: "Phoenix", arrival: "Salt Lake City")
+    @flight5 = Flight.create!(number: "5", date: "01/01/10", time: "2:00 PM", departure: "Las Vegas", arrival: "Bahamas")
     @passenger1 = Passenger.create!(name: "Khoa", age: 35)
     @passenger2 = Passenger.create!(name: "Jen", age: 35)
     @passenger3 = Passenger.create!(name: "Dmytri", age: 7)
@@ -30,6 +30,11 @@ RSpec.describe 'As visitor, when I visit flight index page' do
     expect(page).to have_content("Time")
     expect(page).to have_content("Date")
     expect(page).to have_content("Number")
+    expect(page).to have_content(@flight1.number)
+    expect(page).to have_content(@flight1.date)
+    expect(page).to have_content(@flight1.time)
+    expect(page).to have_content(@flight1.arrival)
+    expect(page).to have_content(@flight1.departure)
 
   end
 
